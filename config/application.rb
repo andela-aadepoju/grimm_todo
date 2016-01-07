571f221cdd9c33df5734044edd8e2f872cdbd428
mradeybee <mradeybee@gmail.com>
2016-01-07 15:31:07 +0100

    add class autoloading

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,5 @@
 require "grimm"
 $LOAD_PATH << File.join(File.dirname(__FILE__), "..", "app", "controllers")
-require "my_pages_controller"
 module GrimmTodo
   class Application < Grimm::Application
   end
