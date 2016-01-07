44a047fa24c0f7cca92b00d5f025df3fad281980
mradeybee <mradeybee@gmail.com>
2016-01-07 13:58:55 +0100

    add render method to controller class

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,6 @@
 require "grimm"
 $LOAD_PATH << File.join(File.dirname(__FILE__), "..", "app", "controllers")
-require "pages_controller"
+require "my_pages_controller"
 module GrimmTodo
   class Application < Grimm::Application
   end
