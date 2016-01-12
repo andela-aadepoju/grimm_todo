e39393f44aa87b6c0c475019006072ea0196e898
mradeybee <mradeybee@gmail.com>
2016-01-12 23:30:46 +0100

    add routing with the request verbs

--- config/application.rb
+++ config/application.rb
@@ -1,5 +1,6 @@
 require "grimm"
-$LOAD_PATH << File.join(File.dirname(__FILE__), "..", "app", "controllers")
+
+# $LOAD_PATH << File.join(File.dirname(__FILE__), "..", "app", "controllers")
 module GrimmTodo
   class Application < Grimm::Application
   end
