fc9dc90ae92bdd179392c1e5aeec36adf7bee677
Adebayo <mradeybee@gmail.com>
2016-01-21 10:57:41 +0100

    modify routing class and css to some pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 GrimmApplication.route.draw do
-  root "my_pages#index"
-  # get "/posts/:id", "posts#show"
-  # post "/new", "my_pages#create"
-  get "posts/edit/:id", "posts#edit"
-  # resources :my_pages
-  # resources :posts
+  root "posts#index"
+  resources :posts
 end
