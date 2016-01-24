052c9bdd6cad9f99aee8b011068d843b76348705
Adebayo <mradeybee@gmail.com>
2016-01-24 23:12:19 +0100

    add models folder and models files, add more flexibility to rendering views

--- app/controllers/my_pages_controller.rb
+++
@@ -1,9 +0,0 @@
-class MyPagesController < Grimm::Controller
-  def index
-    render :index, name: "Adebayo", lastname: "Adepoju"
-  end
-
-  def about
-   render :index, name: "Adebayo", lastname: "Adepoju"
-  end
-end
